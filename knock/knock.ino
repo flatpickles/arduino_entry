f62169cc61d3f6cbfee959416a4632b4ad424e59
Max Lesser <wheels@nyc.rr.com>
2012-10-31 17:22:51 -0400

    version 0.7

--- knock/knock.ino
+++ knock/knock.ino
@@ -1,15 +1,18 @@
 #include <Servo.h>
 
+#define TIMEOUT 5000
+#define THRESHOLD .5
+
 int piezo_pin = 0;
 int led_pin = 13;
 int servo_pin = 12;
 int knock_threshold = 3;
-long debounce_delay = 50;
+long debounce_delay = 100;
 long last;
 Servo s1;
 
-double[5] to_match = {1, 1, 1, 1, 1};
-double[5] input;
+double to_match[4] = {1, 2, 1, 1};
+double input[4];
 int counter;
 double x;
 double temp;
@@ -19,19 +22,21 @@ void setup() {
   pinMode(led_pin, OUTPUT);
   s1.attach(servo_pin);
   
-  // servo test
-  int pos = 0;
-  for (pos = 0; pos < 180; pos++) {
-    s1.write(pos);
-    delay(15);
-  }
-  
   temp = -1;
   counter = 0;
 }
 
 void loop () {
   byte val = analogRead(piezo_pin);
+  
+  int cur_time = millis() - temp;
+  
+  if (temp != -1 && cur_time > TIMEOUT)
+    {
+      counter = 0;
+      temp = -1;
+      Serial.println("timeout");
+    }
   
   if (val >= knock_threshold && last + debounce_delay < millis()) {
     Serial.println("whoooop");
@@ -49,6 +54,8 @@ void loop () {
       int diff = x - temp;
       input[counter] = diff;
       counter++;
+      Serial.println(counter);
+      temp = millis();
     }
     
     
@@ -57,8 +64,65 @@ void loop () {
   }
   
   
-  if(counter == 5)
+  if(counter == 4)
   {
-    // check for validity of knock
+    ratio_input_array(input, 4);
+    
+    int comp = compare_array(to_match, input, 4);
+    
+    if(comp == 1)
+    {
+      s1.write(0);
+      delay(1000);
+      s1.write(180);
+      delay(2000);
+    }
+    
+    temp = -1;
+    counter = 0;
   }
+}
+
+double find_min(double array[], int length){
+       
+        int i = 0;
+        double cur_min = TIMEOUT;
+        for (i = 0; i < length; i++)
+        {
+                if(array[i]<cur_min)
+                        cur_min = array[i];
+        }
+        return cur_min;
+ 
+}
+ 
+void ratio_input_array(double array[], int length){
+       
+        double min_ = find_min (array, length);
+        Serial.println(min_);
+ 
+        int i = 0;
+        for (i = 0; i < length; i++)
+        {
+                array[i] = array[i] / min_;
+                Serial.println(array[i]);
+        }
+}
+ 
+int compare_array(double match_array[], double input_array[], int length)
+{
+        int i = 0;
+        for (int i = 0; i < length; i++){
+                double lower = match_array[i] - THRESHOLD;
+                double higher = match_array[i] + THRESHOLD;
+                if (input_array[i] > higher || input_array[i] < lower)
+                {
+//                    Serial.println(lower);
+//                    Serial.println(higher);
+//                    Serial.println(input_array[i]);
+                    return 0;
+                }
+        }      
+        Serial.println("match");
+        return 1;
 }
