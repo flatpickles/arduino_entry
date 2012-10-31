e3acd3396d8d428afe61fcc7f39711bf2d066626
Max Lesser <wheels@nyc.rr.com>
2012-10-31 16:12:13 -0400

    knock counter

--- knock/knock.ino
+++ knock/knock.ino
@@ -8,6 +8,12 @@ long debounce_delay = 50;
 long last;
 Servo s1;
 
+double[5] to_match = {1, 1, 1, 1, 1};
+double[5] input;
+int counter;
+double x;
+double temp;
+
 void setup() {
   Serial.begin(9600);
   pinMode(led_pin, OUTPUT);
@@ -19,6 +25,9 @@ void setup() {
     s1.write(pos);
     delay(15);
   }
+  
+  temp = -1;
+  counter = 0;
 }
 
 void loop () {
@@ -28,7 +37,28 @@ void loop () {
     Serial.println("whoooop");
     digitalWrite(led_pin, HIGH);
     last = millis();
+    
+    
+    if(temp == -1)
+    {
+      temp = millis();
+    }
+    else
+    {
+      x = millis();
+      int diff = x - temp;
+      input[counter] = diff;
+      counter++;
+    }
+    
+    
   } else if (last + debounce_delay * 3   < millis()) {
     digitalWrite(led_pin, LOW);
+  }
+  
+  
+  if(counter == 5)
+  {
+    // check for validity of knock
   }
 }
